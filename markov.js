0af1bfbb2d4fd11395f790491b77a4ec0dbcb7e2
CallMeAtomfried <63309028+CallMeAtomfried@users.noreply.github.com>
2021-03-11 11:42:19 +0100

    Add files via upload

---
+++ markov.js
@@ -0,0 +1,143 @@
+module.exports = class Markov {
+	
+	constructor(){
+		//idk what could go in here, maybe the models should live in this class instead of being loaded for the main script but it makes no difference, really.
+	};
+	
+	
+	learn(arr, l_context, model, path, fs){
+		
+		//x in arr: Either loop through for every character in string or every word in array of words
+		for(var x in arr){
+			//get next character/word
+			var w2 = arr[parseInt(x)+1];
+			
+			var str = "";
+			//get the previous characters/words depending on specified context size
+			for(var y=l_context-1;y>=0; y--){
+				str = str + (arr[parseInt(x)-y]==undefined?"":arr[parseInt(x)-y]);
+			}
+			
+			 
+			if(model.w[str]==undefined){
+				//if context doesnt exist add datapoint
+				if(x<arr.length-1){
+					
+					model.w[str]={};
+					model.w[str]["t"] = 1;
+					model.w[str]["f"]={};
+					model.w[str]["f"][w2]=1;
+					// console.log(x, parseInt(x)+1, w2);
+				}
+			} else {
+				//add new data to word
+				if(x<arr.length-1){
+					model.w[str].t++;
+					if(model.w[str].f[w2]==undefined) model.w[str].f[w2]=0;
+					model.w[str].f[w2]++;
+				}
+			}
+		}
+		//save updated model
+		fs.writeFile(path, JSON.stringify(model), function(){});
+		//thinking about adding return here
+	}
+	
+	reproduce(length, l_context, model){
+		
+		//get random word to start generating
+		var start = this.randomProperty(model.w);
+		
+		//kinda stupid, afraid to change it
+		var newWord = start;
+		var output = newWord;
+		
+		//unused, can be used to define a bail condition. 
+		var flag = "\r\n";
+		
+		
+		for(var i = start.length; i<length; i++){
+			
+			//Get next character to add onto the output depending on the last l_context characters/words
+			var add = this.getRandom(this.getRanges(output.substr(-l_context), model));
+			
+			if(add==null||add==undefined) {
+				// if no data exist for word, stop generating
+				i = length;
+			} else {
+				// else add new character to output
+				output = output + add;
+				
+			}
+		}
+		
+		return output;
+		
+		
+	}
+	
+	 getRanges(word, words){
+		
+		//failsave: if no data exist for the last i characters, reduce the context size by 1 until it works.
+		//changes behaviour drastically and isnt pretty in these cases, but it gives longer results
+		for(var i = 0; i < 6; i++){
+			if(words.w[word]==undefined){
+				word = word.substring(1);
+			}
+		}
+		
+		if(word.length==0){
+			//return null if nothing is found
+			return null;
+		} else {
+			//Get the total amount of characters following the word. (Sum of all probabilities for each valid character)
+			var total = words.w[word]["t"];
+			//failsave
+			if(total==undefined) total = 1;
+			//get the map of all characters following a word and the amount of occurences
+			var followup = words.w[word].f;
+			var nw = word;
+			//another failsave that tries to get a random word to continue checking. again, unfavorable but it saves the whole thing with smaller models
+			while(followup==undefined){
+				nw = nw.substr(1);
+				followup = words.w[nw].f;
+				
+				if(nw="") followup = this.randomProperty(words.w);
+			}
+			//get the characters that follow the word
+			var ranges = [];
+			var keys = Object.keys(words.w[word].f);
+			var totalrange = 0;
+			//calculate the probabilities
+			for(var x in keys){
+				ranges[x]=[keys[x], totalrange+(parseFloat(followup[keys[x]])/parseFloat(total))];
+				totalrange+=(parseFloat(followup[keys[x]])/parseFloat(total));
+			}
+			return ranges;
+		}
+	}
+	
+	
+	 randomProperty(object) {
+		 //returns a random key
+		  var keys = Object.keys(object);
+		  return keys[Math.floor(keys.length * Math.random())];
+	}
+
+	getRandom(ranges){
+		if(ranges==null){
+			//stop if getRanges returns null
+			return null;
+		}
+		//get a weighted random entry from the ranges generated in getRanges
+		var rand = Math.random();
+		for(var x in ranges){
+			if(rand<ranges[x][1]) return ranges[x][0];
+		}
+		
+	}
+
+}
+
+
+
