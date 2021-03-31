3c8ebcef37e1f9ca1bbbfe6824d0e06dd2e534a0
CallMeAtomfried <63309028+CallMeAtomfried@users.noreply.github.com>
2021-03-31 12:41:10 +0200

    Create markov.py

---
+++ markov.py
@@ -0,0 +1,20 @@
+#Markov.js port for Python (WIP)
+
+class Markov:
+  model = {} 
+  
+  def learn(inputData, contextSize):
+    #The learning
+
+  def reproduce(textLength, contextSize):
+    output = "" 
+    #reproduction stuff
+    
+    return output
+
+  def getRandom(ranges):
+    return 0 #return random element from ranges
+
+  def getRanges():
+    return 0 #collect and return the ranges
+    
